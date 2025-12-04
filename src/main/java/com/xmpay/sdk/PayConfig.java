0eccc190b77e53d76b8d1dd88fba53fb68fca919
zhangyuan <zhangyuan@novgate.com>
2025-12-04 13:52:02 +0800

    init

--- src/main/java/com/xmpay/sdk/PayConfig.java
+++ src/main/java/com/xmpay/sdk/PayConfig.java
@@ -1,13 +1,18 @@
 package com.xmpay.sdk;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PayConfig implements Serializable {
 
     /**
